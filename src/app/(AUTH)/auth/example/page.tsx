63aaeded4bd256585d932876f61b39e45aef3f87
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-07-18 14:20:11 -0300

    ci: react query configure

---
+++ src/app/(AUTH)/auth/example/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <main>
+        <h1>Private page example</h1>
+    </main>
+  )
+}
---
+++ src/components/providers/query-provider.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react';
+import {ReactNode} from 'react'
+import {QueryClient, QueryClientProvider} from "react-query"
+
+interface props{
+    children: ReactNode;
+}
+export default function QueryProvider({children}:props) {
+    const QC = new QueryClient();
+  return (
+    <QueryClientProvider client={QC}>
+      {children}
+    </QueryClientProvider>
+  )
+}
