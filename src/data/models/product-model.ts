9c7392564cd3492c2e28092072c3d089b2eee62f
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-07-18 13:42:49 -0300

    ci: zustand configure

--- src/data/models/product-model.ts
+++ src/data/models/product-model.ts
@@ -1,13 +1,20 @@
 import z from "zod";
 
 export interface Product{
+    id: number,
     name: string;
     description: string;
     image: string;
     value: number;
 }
 
+export interface ProductsStore{
+    products: Product[],
+    addProduct: (item:Product) => void;
+    removeItem: (id:number)=>void;
+}
 export const ProductSchema = z.object({
+    id: z.number(),
     name: z.string(),
     description: z.string(),
     image: z.string().url(),
---
+++ src/data/storage/products-store.ts
@@ -0,0 +1,12 @@
+import { create } from 'zustand'
+import { ProductsStore } from '../models/product-model'
+
+export const ProductStorage = create<ProductsStore>((set, get) => ({
+  products: [],
+  addProduct: (product) => set((state) => ({ products: [...state.products, product] })),
+  removeItem: (id) => {
+    const products = get().products;
+    const filtered = products.filter((i)=>i.id!=id);
+    set({ products: filtered})
+  }}
+))
