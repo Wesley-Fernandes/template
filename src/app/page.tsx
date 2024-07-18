637e639ca28d705d6fb9708eb2ef31a63e3141d8
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-07-18 14:25:53 -0300

    ci: fix prettier commands

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-
-import React from "react";
+import React from 'react';
 
 export default function Home() {
+  const version = process.env.NEXT_TEMPLATE_VERSION;
   return (
     <main>
-      <h1>NextJS {process.env.NEXT_TEMPLATE_VERSION}</h1>
+      <h1>NextJS: {version}</h1>
     </main>
   );
 }
